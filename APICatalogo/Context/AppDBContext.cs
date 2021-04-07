c3e9e1d08316dc9121c920407b46e4aee22c7e4d
WellingtonFR <gbwellington@hotmail.com>
2021-04-06 23:54:40 -0300

    Adicionar arquivos de projeto.

---
+++ APICatalogo/Context/AppDBContext.cs
@@ -0,0 +1,19 @@
+﻿using APICatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace APICatalogo.Context
+{
+    public class AppDBContext : DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+    }
+}
---
+++ APICatalogo/Migrations/20210407021924_PopulaDB.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APICatalogo.Migrations
+{
+    public partial class PopulaDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Insert into Categorias(Nome,ImagemUrl) values('bebidas',"+"'https://www.supermuffato.com.br/logo.png')");
+            migrationBuilder.Sql("Insert into Categorias(Nome,ImagemUrl) values('Lanches',"+"'https://www.supermuffato.com.br/logo.png')");
+            migrationBuilder.Sql("Insert into Categorias(Nome,ImagemUrl) values('Sobremesas',"+"'https://www.supermuffato.com.br/logo.png')");
+            
+            migrationBuilder.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,"
+                + "DataCadastro,CategoriaId) Values('Coca Cola','Refrigerante de cola 350ml',"
+                + "5.45,'https://www.supermuffato.com.br/coca.png',50,now()," +
+                "(Select CategoriaId from Categorias where Nome='Bebidas')" +
+                ")");
+            migrationBuilder.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,"
+                + "DataCadastro,CategoriaId) Values('Coca Cola','Refrigerante de cola 350ml',"
+                + "5.45,'https://www.supermuffato.com.br/coca.png',50,now()," +
+                "(Select CategoriaId from Categorias where Nome='Lanches')" +
+                ")");
+            migrationBuilder.Sql("Insert into Produtos(Nome,Descricao,Preco,ImagemUrl,Estoque,"
+                + "DataCadastro,CategoriaId) Values('Coca Cola','Refrigerante de cola 350ml',"
+                + "5.45,'https://www.supermuffato.com.br/coca.png',50,now()," +
+                "(Select CategoriaId from Categorias where Nome='Sobremesas')" +
+                ")");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("Delete from Categorias");
+            migrationBuilder.Sql("Delete from Produtos");
+        }
+    }
+}
